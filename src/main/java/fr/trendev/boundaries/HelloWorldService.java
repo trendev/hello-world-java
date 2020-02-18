b91b791520e5676d1f10d0ea63d83608153f31a9
Julien Sié <julien.sie@gmail.com>
2020-02-19 00:28:43 +0100

    implement HelloWorld service

--- src/main/java/fr/trendev/boundaries/HelloWorldService.java
+++ src/main/java/fr/trendev/boundaries/HelloWorldService.java
@@ -1,20 +1,37 @@
 package fr.trendev.boundaries;
 
+import java.util.Date;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.json.Json;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 /**
  *
- * @author 
+ * @author
  */
 @Path("hello-world")
+@RequestScoped
 public class HelloWorldService {
-    
+
+    @Inject
+    @ConfigProperty(name = "TEXT_MESSAGE", defaultValue = "Hello World")
+    private String message;
+
     @GET
-    public Response ping(){
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response ping() {
         return Response
-                .ok("ping")
+                .ok(Json.createObjectBuilder()
+                        .add("message", message)
+                        .add("timestamp", new Date().getTime())
+                        .build()
+                )
                 .build();
     }
 }
