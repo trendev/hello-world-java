bc74fac5d452fb1e45c9f89972fac7137a2b1404
Julien Sié <julien.sie@gmail.com>
2020-02-19 02:10:53 +0100

    inject pod information

--- src/main/java/fr/trendev/boundaries/HelloWorldService.java
+++ src/main/java/fr/trendev/boundaries/HelloWorldService.java
@@ -22,6 +22,18 @@ public class HelloWorldService {
     @Inject
     @ConfigProperty(name = "TEXT_MESSAGE", defaultValue = "NO_MESSAGE_SET")
     private String message;
+    
+    @Inject
+    @ConfigProperty(name = "MY_POD_NAME")
+    private String podName;
+    
+    @Inject
+    @ConfigProperty(name = "MY_POD_NAMESPACE")
+    private String namespace;
+    
+    @Inject
+    @ConfigProperty(name = "MY_POD_IP")
+    private String podIP;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -29,6 +41,9 @@ public class HelloWorldService {
         return Response
                 .ok(Json.createObjectBuilder()
                         .add("message", message)
+                        .add("pod_name",podName)
+                        .add("namespace",namespace)
+                        .add("pod_IP",podIP)
                         .add("timestamp", new Date().getTime())
                         .build()
                 )
