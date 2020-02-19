a1c9d2bb072fa14d0c5833677ee2f38788f0fe96
Julien Sié <julien.sie@gmail.com>
2020-02-19 15:44:33 +0100

    reduce path + clean dependencies

--- src/main/java/fr/trendev/JAXRSConfiguration.java
+++ src/main/java/fr/trendev/JAXRSConfiguration.java
@@ -7,7 +7,7 @@ import javax.ws.rs.core.Application;
  * Configures JAX-RS for the application.
  * @author Juneau
  */
-@ApplicationPath("api")
+@ApplicationPath("/")
 public class JAXRSConfiguration extends Application {
     
 }
