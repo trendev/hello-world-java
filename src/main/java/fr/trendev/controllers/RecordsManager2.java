214f518ee8cdc723575652a1a91a3f56863b5674
Julien Sié <julien.sie@gmail.com>
2020-02-27 20:12:34 +0100

    use Lock

--- src/main/java/fr/trendev/controllers/RecordsManager2.java
+++ src/main/java/fr/trendev/controllers/RecordsManager2.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
@@ -24,10 +25,10 @@ import org.eclipse.microprofile.config.ConfigProvider;
  *
  * @author jsie
  */
-@HazelcastReplicatedMap
 @ApplicationScoped
+@HazelcastReplicatedMap
 public class RecordsManager2 implements RecordsManager {
-    
+
     private HazelcastInstance hz;
 
     private ReplicatedMap<String, LinkedList<String>> map;
@@ -74,18 +75,30 @@ public class RecordsManager2 implements RecordsManager {
     @Override
     public List<String> add(String value) {
 
-        LinkedList<String> records = this.map.getOrDefault(key, new LinkedList<>());
+        LinkedList<String> records = new LinkedList<>();
 
-        //pop old entries
-        if (records.size() >= maxSize) {
-            while (records.size() != maxSize - 1) {
-                records.remove();
+        // replace with FencedLock when CP system will be enabled
+        Lock hzLock = hz.getLock(this.getClass().getName());
+        hzLock.lock();
+
+        try {
+            records = this.map.getOrDefault(key, new LinkedList<>());
+
+            //pop old entries
+            if (records.size() >= maxSize) {
+                while (records.size() != maxSize - 1) {
+                    records.remove();
+                }
             }
-        }
-        records.add(value);
+            records.add(value);
 
-        this.map.put(key, records);
+            this.map.put(key, records);
+
+        } finally {
+            hzLock.unlock();
+        }
 
         return Collections.unmodifiableList(records);
+
     }
 }
