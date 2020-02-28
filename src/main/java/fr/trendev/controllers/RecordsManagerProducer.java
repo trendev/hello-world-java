9062fd9805443cada55686875284aa87cdf31965
Julien Sié <julien.sie@gmail.com>
2020-02-28 17:30:25 +0100

    inject CLUSTERED_OBJECT_TYPE env var

--- src/main/java/fr/trendev/controllers/RecordsManagerProducer.java
+++ src/main/java/fr/trendev/controllers/RecordsManagerProducer.java
@@ -5,10 +5,15 @@
  */
 package fr.trendev.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
+import javax.ws.rs.WebApplicationException;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 /**
@@ -17,29 +22,71 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
  */
 @ApplicationScoped
 public class RecordsManagerProducer {
-
+    
     private final static String HAZELCAST_REPLICATED_MAP = "hazelcast-replicated-map";
     private final static String CLUSTERED_SINGLETON = "clustered-singleton";
-
+    
     @Inject
     @ConfigProperty(name = "CLUSTERED_OBJECT_TYPE", defaultValue = HAZELCAST_REPLICATED_MAP)
     private String clusterObjectType;
-
+    
     @Inject
     @ClusteredSingleton
     private RecordsManager rm1;
-
+    
     @Inject
     @HazelcastReplicatedMap
     private RecordsManager rm2;
-
+    
+    private RecordsManager rm;
+    
+    private List<RecordsManagerSelector> selectors;
+    
+    @PostConstruct
+    public void init() {
+        selectors = Arrays.asList(
+                new RecordsManagerSelector(CLUSTERED_SINGLETON, rm1),
+                new RecordsManagerSelector(HAZELCAST_REPLICATED_MAP, rm2));
+    }
+    
     @Produces
     @Default
     public RecordsManager getRecordsManager() {
-        if (CLUSTERED_SINGLETON.equals(this.clusterObjectType)) {
-            return rm1;
+        
+        if (this.rm == null) {
+            this.rm = selectors.stream()
+                    .map(rms -> rms.select(clusterObjectType))
+                    .filter(o -> o.isPresent())
+                    .findFirst()
+                    .map(o -> o.get())
+                    .orElseThrow(() -> 
+                            new WebApplicationException("clusterObjectType=" 
+                                    + clusterObjectType 
+                                    + " : it is not a supported clustered object type!"));
         }
-        return rm2;
+        
+        return this.rm;
+        
     }
-
+    
+    private class RecordsManagerSelector {
+        
+        private final String type;
+        private final RecordsManager rm;
+        
+        RecordsManagerSelector(String type, RecordsManager rm) {
+            this.type = type;
+            this.rm = rm;
+        }
+        
+        Optional<RecordsManager> select(String type) {
+            if (this.type.equals(type)) {
+                return Optional.of(rm);
+            } else {
+                return Optional.empty();
+            }
+            
+        }
+        
+    }
 }
